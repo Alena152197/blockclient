0333c87dd884829e4770cb58d10ef1828390e5cc
Furajkova Alena <Furajlova@mail.ru>
2024-11-19 20:35:07 +0300

    дом

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script setup>
-const base = 'http://https://panel.4cce9171d57c.vps.myjino.ru'
+const base = 'http://panel.4cce9171d57c.vps.myjino.ru'
 const api = await $fetch(`${base}/api/config?populate=*`)
 const config = api.data
 
