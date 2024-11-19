53bb25e0b850337d9370369885be63b49ab42c12
Furajkova Alena <Furajlova@mail.ru>
2024-11-19 18:43:24 +0300

    дом

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <NuxtLoadingIndicator throttle="0" />
+  <Navbar />
+  <NuxtPage />
+  <Up />
+  <Footer />
+</template>
+
+<script setup>
+const base = 'http://localhost:1337'
+const api = await $fetch(`${base}/api/config?populate=*`)
+const config = api.data
+
+useHead({
+  title: config.title,
+  meta: [
+    { name: 'description', content: config.desc },
+    { name: 'keywords', content: config.keywords },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: base+config.favicon.url }
+  ],
+  bodyAttrs: {
+    class: 'container mx-auto'
+  },
+})
+
+</script>
+
+
