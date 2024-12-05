5bc191986cad4998e957882025e71c0d44b9cae9
Furajkova Alena <Furajlova@mail.ru>
2024-12-05 21:08:38 +0300

    *

--- pages/search.vue
+++ pages/search.vue
@@ -1,30 +1,28 @@
 <template>
     <h1>hello</h1>
     <main>
-        <article v-for="post in posts" :key="post.Required"></article>
+        <article v-for="post in posts" :key="post.Required">
+            <h3>{{ post.title }}</h3>
+        </article>
     </main>
 </template>
 
 <script setup>
-import { useSearchStore } from '~/stores/search';
-
 
 const search = useSearchStore()
 const posts = ref()
 
-async function searcAlert() {
+async function searchAlert() {
     console.log(search.searchQuery);
 
     try {
         const response = await $fetch('http://324cbb377ef9.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${search.searchQuery}&filters[$or][1][body][$containsi]=${search.searchQuery}')
-        const searchResponse = response.data;
+        posts = response.data;
     } catch (error) {
         console.error('bla bla bla', error);
     }
 }
 
-onMounted(() => {
-    searcAlert()
-})
+onMounted(() => searcAlert())
 
 </script>
