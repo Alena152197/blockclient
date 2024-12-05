3d4e63d5fa779fde55dce0028ddfbb4b6b6cd273
Furajkova Alena <Furajlova@mail.ru>
2024-12-05 20:57:26 +0300

    *

--- pages/search.vue
+++ pages/search.vue
@@ -1,10 +1,23 @@
 <template>
     <h1>hello</h1>
+    <main>
+        <button @click="searcAlert">click</button>
+        {{ search }}
+    </main>
 </template>
 
 <script setup>
+import { useSearchStore } from '~/stores/search';
 
-const stringSearch = ref('')
+
+const search = useSearchStore()
+
+function searcAlert() {
+    console.log(search.searchQuery);
+    
+}
+
 const response = await $fetch('http://324cbb377ef9.vps.myjino.ru/api/posts?filters[$or][0][title][$containsi]=${stringSearch}&filters[$or][1][body][$containsi]=${stringSearch}')
+const searchResponse = response.data
 
 </script>
