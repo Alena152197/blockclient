1a149489ef10d7979ec3f89eb5df6da4a8187bdd
Furajkova Alena <Furajkova@mail.ru>
2024-12-17 01:17:49 +0300

    *

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -17,7 +17,7 @@
 
 
     <main class="max-w-screen-xl min-w-96 md:min-w-[48rem] lg:min-w-[56rem] mx-auto py-2">
-            <!--хлебные крошки-->
+        <!--хлебные крошки-->
         <nav class="flex" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                 <li class="inline-flex items-center">
@@ -52,18 +52,24 @@
                                 d="m1 9 4-4-4-4" />
                         </svg>
                         <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400"><strong>{{
-                                post.title }}</strong></span>
+                            post.title }}</strong></span>
                     </div>
                 </li>
             </ol>
         </nav>
 
 
-        <div class="h-96 rounded 2xl my-10 bg-fixed bg-[length:90%_600px]"
+        <div class="bg-no-repeat bg-top h-96 rounded-se-2xl my-10 bg-fixed bg-[length:40%_800px]"
             :style="'background-image: url(' + base_url + post.img[0].url + ')'"></div>
-        <strong><h1>{{ post.title }}</h1></strong>
-        <p class="date">Дата публикации: <span>{{ post.publishedAt }}</span> </p> <br>
-        <div v-html="mark"></div>
+        <div class="px-8">
+            <strong>
+                <h1>{{ post.title }}</h1>
+            </strong>
+            <p class="date">Дата публикации: <span>{{ post.publishedAt }}</span> </p> <br>
+        </div>
+
+        <div class="dateText" v-html="mark"></div>
+
         <!-- <img :src=base_url+post.img[0].url :alt=post.img[0].alternativeText> -->
     </main>
 </template>
