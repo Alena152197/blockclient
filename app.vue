b8865b7b819bc9404b92c6763474e5ccee8b8ebf
Furajkova Alena <Furajkova@mail.ru>
2024-12-27 18:01:25 +0300

    fix *

--- app.vue
+++ app.vue
@@ -1,42 +1,35 @@
 <template>
-  <!-- <NuxtLoadingIndicator throttle="0" /> -->
-  <!-- <Navbar /> -->
-  <!-- <NuxtPage />
+  <NuxtLoadingIndicator throttle="0" />
+  <Navbar />
+  <NuxtPage />
   <Up />
-  <Footer /> -->
-  <button @click="funcfefefe()">КНопка</button>
+  <Footer />
 </template>
 
 <script setup>
-// const base = 'https://324cbb377ef9.vps.myjino.ru'
-// const api = await $fetch(`${base}/api/config?populate=*`)
-// const config = api.data
+const base = 'https://324cbb377ef9.vps.myjino.ru'
+const api = await $fetch(`${base}/api/config?populate=*`)
+const config = api.data
 
-// useHead({
-//   title: config.title,
-//   meta: [
-//     { name: 'description', content: config.desc },
-//     { name: 'keywords', content: config.keywords },
-//   ],
-//   link: [
-//     { rel: 'icon', type: 'image/x-icon', href: base+config.favicon.url }
-//   ],
-//   // настройка боди на главной
-//   bodyAttrs: {
-//     class: 'bg-white dark:bg-gray-900'
-//   },
-//   head: {
-//     script: [
-//       { src:'https://yastatic.net/share2/share.js', async: true, defer: true }
-//     ]
-//   }
-// })
-
-function funcfefefe() {
-  console.log('fewfewfew');
-}
-
-onMounted(() => console.log('erfefe'))
+useHead({
+  title: config.title,
+  meta: [
+    { name: 'description', content: config.desc },
+    { name: 'keywords', content: config.keywords },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: base+config.favicon.url }
+  ],
+  // настройка боди на главной
+  bodyAttrs: {
+    class: 'bg-white dark:bg-gray-900'
+  },
+  head: {
+    script: [
+      { src:'https://yastatic.net/share2/share.js', async: true, defer: true }
+    ]
+  }
+})
 </script>
 
 
