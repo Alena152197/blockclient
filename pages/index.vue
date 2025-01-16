5cf70bcc8bb0596c935703b5f5ef5c63b083a0e0
Furajkova Alena <Furajlova@mail.ru>
2025-01-16 20:48:18 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -12,11 +12,22 @@
                     src="../public/img/блокнот_зебра.png" alt="блокнот">
             </div>
         </div>
-        <div class="relative flex justify-center items-center mt-[-20px] left-[490px]"> <!-- Отрицательный отступ для наложения -->
+        <div class="relative flex justify-center items-center mt-[-20px] left-[490px]">
+            <!-- Отрицательный отступ для наложения -->
             <img class="absolute ml-32 mb-[220px]" src="../public/img/завиток_большой.png" alt="завиток">
-            <img class="absolute mt-26 mr-10 md:w-[247px] md:h-[150px]" src="../public/img/карандаш_top.png" alt="карандаш_top">
+            <img class="absolute mt-26 mr-10 md:w-[247px] md:h-[150px]" src="../public/img/карандаш_top.png"
+                alt="карандаш_top">
             <img class="absolute ml-40 mb-8 md:w-[247px] md:h-[150px]" src="../public/img/карандаш.png" alt="карандаш">
         </div>
+        <!-- <div class="relative flex justify-center transform -translate-x-1/2 -translate-y-1/2">
+            <div class=" translate-x-[120px] md:-translate-x-[150px] scale-65 lg:scale-50">
+                <img class="absolute" src="../public/img/графит.png" alt="">
+                <img class="absolute" src="../public/img/точиолка.png" alt="">
+                <img class="absolute" src="../public/img/завиток_niz.png" alt="">
+                <img class="absolute" src="../public/img/завиток_top.png" alt="">
+            </div>
+        </div> -->
+
 
         <!-- <blockquote class="blockquote-7">
             <p>"Мы все, как карандаши. <br>
