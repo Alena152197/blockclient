b2ed4a2853634f39a296ae27347afe952a8bb9cf
Furajkova Alena <Furajkova@mail.ru>
2025-01-18 06:43:58 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,82 @@
 <template>
     <div class="fonImage">
-        <div class="flex justify-center items-center h-full">
+        <div class="grid-container absolute">
+            <div
+                class="grid-item item1 absolute col-start-2 col-end-3 row-start-1 row-end-3  justify-items-center border-2 border-sky-500">
+                <img src="../public/img/блокнот_зебра.png" alt="" width="940px">
+            </div>
+            <div
+                class="grid-item item2 relative col-start-1 col-end-1 row-start-1 row-end-4 justify-items-end border-2 border-sky-500">
+                <img src="../public/img/блокнот_море.png" alt="" width="400px">
+                <img src="../public/img/блокнот_горы.png" alt="" width="400px">
+            </div>
+
+            <div class="grid-item item3 col-start-2 col-end-3 row-start-3 row-end-3 justify-items-start border-2 border-sky-500 relative"> 
+                <img src="../public/img/графит.png" alt="" width="200px" class="absolute top-10 left-10">
+                <img src="../public/img/точиолка.png" alt="" width="56px" class="absolute top-[140px] left-20">
+                <img src="../public/img/завиток_niz.png" alt="" width="51px" class="absolute top-[150px] left-[200px]">
+                <img src="../public/img/завиток_top.png" alt="" width="54px" class="absolute top-[140px] left-[140px]">
+
+            </div>
+
+            <div
+                class="grid-item item4 relative col-start-2 col-end-3 row-start-3 row-end-3 justify-items-end border-2 border-sky-500">
+                <img src="../public/img/завиток_большой.png" alt="" width="100px">
+                <img src="../public/img/карандаш_top.png" alt="" width="100px">
+                <img src="../public/img/карандаш.png" alt="" width="100px">
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+
+
+<!-- <div class="flex justify-center items-center h-full">
             <div class="absolute top-[53%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 ">
-                <div class=" -translate-x-[320px] md:-translate-x-[350px] scale-65 lg:scale-90">
-                    <img src="../public/img/блокнот_море.png" alt="море">
-                    <img src="../public/img/блокнот_горы.png" alt="горы">
+                <div class="flex flex-wrap justify-center space-x-4">
+                    <div class="box_1 -translate-x-[320px] md:-translate-x-[350px] scale-65 lg:scale-90">
+                        <img src="../public/img/блокнот_море.png" alt="Image_1" class="w-96 h-auto mb-4">
+                        <img src="../public/img/блокнот_горы.png" alt="Image_2" class="w-96 h-auto mb-4">
+                    </div>
+                    <div class="box_2 -translate-x-[20px] md:-translate-y-[-100px] scale-65 lg:scale-90">
+                        <img src="../public/img/графит.png" alt="Image_6" class="w-32 h-auto">
+                        <img src="../public/img/точиолка.png" alt="Image_7" class="w-32 h-auto">
+                        <img src="../public/img/завиток_niz.png" alt="Image_8" class="w-32 h-auto">
+                        <img src="../public/img/завиток_top.png" alt="Image_9" class="w-32 h-auto">
+                    </div>
+                    <div class="box_3">
+                        <img src="../public/img/завиток_большой.png" alt="Image_3" class="w-32 h-auto mb-4">
+                        <img src="../public/img/карандаш_top.png" alt="Image_4" class="w-32 h-auto mb-4">
+                        <img src="../public/img/карандаш.png" alt="Image_5" class="w-32 h-auto mb-4">
+                    </div>
+
+
                 </div>
             </div>
-            <div class="relative z-10 left-44 h-auto">
-                <img class="blockNotImg w-full mb-[70px] sm:w-[650px] md:w-[981px] md:h-[600px]"
-                    src="../public/img/блокнот_зебра.png" alt="блокнот">
-            </div>
-        </div>
-        <div class="relative flex justify-center items-center mt-[-20px] left-[490px]">
-            <!-- Отрицательный отступ для наложения -->
-            <img class="absolute ml-32 mb-[220px]" src="../public/img/завиток_большой.png" alt="завиток">
-            <img class="absolute mt-26 mr-10 md:w-[247px] md:h-[150px]" src="../public/img/карандаш_top.png"
-                alt="карандаш_top">
-            <img class="absolute ml-40 mb-8 md:w-[247px] md:h-[150px]" src="../public/img/карандаш.png" alt="карандаш">
-        </div>
-        <!-- <div class="relative flex justify-center transform -translate-x-1/2 -translate-y-1/2">
+        </div> -->
+
+<!-- <div class="flex space-x-4">
+                    <div class="-translate-x-[320px] md:-translate-x-[350px] scale-65 lg:scale-90">
+                        <img src="../public/img/блокнот_море.png" alt="море">
+                        <img src="../public/img/блокнот_горы.png" alt="горы">
+                    </div>
+                    <div class="flex ">
+                        <div class="ml-20 -translate-x-[320px] md:-translate-x-[-350px] md:-translate-y-[-450px] scale-65 lg:scale-90">
+                            <img src="../public/img/завиток_большой.png" alt="завиток">
+                            <img src="../public/img/карандаш_top.png" alt="карандаш_top">
+                            <img src="../public/img/карандаш.png" alt="карандаш">
+                        </div>
+                    </div>
+                </div> -->
+
+
+
+
+
+
+<!-- <div class="relative flex justify-center transform -translate-x-1/2 -translate-y-1/2">
             <div class=" translate-x-[120px] md:-translate-x-[150px] scale-65 lg:scale-50">
                 <img class="absolute" src="../public/img/графит.png" alt="">
                 <img class="absolute" src="../public/img/точиолка.png" alt="">
@@ -29,12 +86,10 @@
         </div> -->
 
 
-        <!-- <blockquote class="blockquote-7">
+<!-- <blockquote class="blockquote-7">
             <p>"Мы все, как карандаши. <br>
                 Каждый рисует свою судьбу. <br>
                 Один ломается, другой тупит, <br>
                 а кто-то затачивается и движется вперёд".</p>
             <cite class="cite">Светлана Кременецкая</cite>
         </blockquote> -->
-    </div>
-</template>
