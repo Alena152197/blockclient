c4dafb9dccc59d002a4a42e8653a9b06fddeab3b
Furajkova Alena <Furajkova@mail.ru>
2025-01-18 09:48:43 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="flex justify-center self-center h-full m-auto">
         <div class="grid-container">
-            <div class="grid-item item1">1</div>
-            <div class="grid-item item2">2</div>
+            <div class="grid-item item1">
+                <img src="../public/img/блокнот_море.png" alt="">
+            </div>
+            <div class="grid-item item2">
+                <img src="../public/img/блокнот_зебра.png" alt="">
+            </div>
             <div class="grid-item item3">3</div>
-            <div class="grid-item item4">4</div>
-            <div class="grid-item item5">5</div>
-            <div class="grid-item item6">6</div>
-            <div class="grid-item item7">7</div>
+            <div class="grid-item item4">
+                <img src="../public/img/графит.png" alt="">
+                <img src="../public/img/точиолка.png" alt="">
+                <img src="../public/img/завиток_top.png" alt="">
+                <img src="../public/img/завиток_niz.png" alt="">
+            </div>
+            <div class="grid-item item5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ratione rerum suscipit ad error quas commodi quasi similique. Reprehenderit reiciendis tempora pariatur perspiciatis incidunt sed id quod numquam nesciunt perferendis.</div>
+            <div class="grid-item item6">
+                <img src="../public/img/блокнот_горы.png" alt="">
+            </div>
+            <div class="grid-item item7">
+                <img src="../public/img/завиток_большой.png" alt="">
+                <img src="../public/img/карандаш_top.png" alt="">
+                <img src="../public/img/карандаш.png" alt="">
+            </div>
             <div class="grid-item item8">8</div>
             <div class="grid-item item9">9</div>
             <div class="grid-item item10">10</div>
