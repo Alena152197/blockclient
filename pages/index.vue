67887ea0f017ddcce9f82cf10c4fd8908420c909
Furajkova Alena <Furajkova@mail.ru>
2025-01-18 09:25:10 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,22 @@
 <template>
-    <div class="fonImage">
-        <div class="grid-container absolute">
-            <div
-                class="grid-item item1 absolute col-start-2 col-end-3 row-start-1 row-end-3  justify-items-center border-2 border-sky-500">
-                <img src="../public/img/блокнот_зебра.png" alt="" width="940px">
-            </div>
-            <div
-                class="grid-item item2 relative col-start-1 col-end-1 row-start-1 row-end-4 justify-items-end border-2 border-sky-500">
-                <img src="../public/img/блокнот_море.png" alt="" width="400px">
-                <img src="../public/img/блокнот_горы.png" alt="" width="400px">
-            </div>
-
-            <div class="grid-item item3 col-start-2 col-end-3 row-start-3 row-end-3 justify-items-start border-2 border-sky-500 relative"> 
-                <img src="../public/img/графит.png" alt="" width="200px" class="absolute top-10 left-10">
-                <img src="../public/img/точиолка.png" alt="" width="56px" class="absolute top-[140px] left-20">
-                <img src="../public/img/завиток_niz.png" alt="" width="51px" class="absolute top-[150px] left-[200px]">
-                <img src="../public/img/завиток_top.png" alt="" width="54px" class="absolute top-[140px] left-[140px]">
-
-            </div>
-
-            <div
-                class="grid-item item4 relative col-start-2 col-end-3 row-start-3 row-end-3 justify-items-end border-2 border-sky-500">
-                <img src="../public/img/завиток_большой.png" alt="" width="100px">
-                <img src="../public/img/карандаш_top.png" alt="" width="100px">
-                <img src="../public/img/карандаш.png" alt="" width="100px">
-            </div>
+    <div class="flex justify-center self-center h-full m-auto">
+        <div class="grid-container">
+            <div class="grid-item item1">1</div>
+            <div class="grid-item item2">2</div>
+            <div class="grid-item item3">3</div>
+            <div class="grid-item item4">4</div>
+            <div class="grid-item item5">5</div>
+            <div class="grid-item item6">6</div>
+            <div class="grid-item item7">7</div>
+            <div class="grid-item item8">8</div>
+            <div class="grid-item item9">9</div>
+            <div class="grid-item item10">10</div>
+            
         </div>
     </div>
 </template>
 
 
-
-
-
-<!-- <div class="flex justify-center items-center h-full">
-            <div class="absolute top-[53%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 ">
-                <div class="flex flex-wrap justify-center space-x-4">
-                    <div class="box_1 -translate-x-[320px] md:-translate-x-[350px] scale-65 lg:scale-90">
-                        <img src="../public/img/блокнот_море.png" alt="Image_1" class="w-96 h-auto mb-4">
-                        <img src="../public/img/блокнот_горы.png" alt="Image_2" class="w-96 h-auto mb-4">
-                    </div>
-                    <div class="box_2 -translate-x-[20px] md:-translate-y-[-100px] scale-65 lg:scale-90">
-                        <img src="../public/img/графит.png" alt="Image_6" class="w-32 h-auto">
-                        <img src="../public/img/точиолка.png" alt="Image_7" class="w-32 h-auto">
-                        <img src="../public/img/завиток_niz.png" alt="Image_8" class="w-32 h-auto">
-                        <img src="../public/img/завиток_top.png" alt="Image_9" class="w-32 h-auto">
-                    </div>
-                    <div class="box_3">
-                        <img src="../public/img/завиток_большой.png" alt="Image_3" class="w-32 h-auto mb-4">
-                        <img src="../public/img/карандаш_top.png" alt="Image_4" class="w-32 h-auto mb-4">
-                        <img src="../public/img/карандаш.png" alt="Image_5" class="w-32 h-auto mb-4">
-                    </div>
-
-
-                </div>
-            </div>
-        </div> -->
-
-<!-- <div class="flex space-x-4">
-                    <div class="-translate-x-[320px] md:-translate-x-[350px] scale-65 lg:scale-90">
-                        <img src="../public/img/блокнот_море.png" alt="море">
-                        <img src="../public/img/блокнот_горы.png" alt="горы">
-                    </div>
-                    <div class="flex ">
-                        <div class="ml-20 -translate-x-[320px] md:-translate-x-[-350px] md:-translate-y-[-450px] scale-65 lg:scale-90">
-                            <img src="../public/img/завиток_большой.png" alt="завиток">
-                            <img src="../public/img/карандаш_top.png" alt="карандаш_top">
-                            <img src="../public/img/карандаш.png" alt="карандаш">
-                        </div>
-                    </div>
-                </div> -->
-
-
-
-
-
-
-<!-- <div class="relative flex justify-center transform -translate-x-1/2 -translate-y-1/2">
-            <div class=" translate-x-[120px] md:-translate-x-[150px] scale-65 lg:scale-50">
-                <img class="absolute" src="../public/img/графит.png" alt="">
-                <img class="absolute" src="../public/img/точиолка.png" alt="">
-                <img class="absolute" src="../public/img/завиток_niz.png" alt="">
-                <img class="absolute" src="../public/img/завиток_top.png" alt="">
-            </div>
-        </div> -->
 
 
 <!-- <blockquote class="blockquote-7">
