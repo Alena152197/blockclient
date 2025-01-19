e5a456a45bc66350771e67423fb3ebb692756898
Furajkova Alena <Furajkova@mail.ru>
2025-01-19 06:54:22 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="flex justify-center self-center h-full m-auto">
-        <div class="grid-container">
+    <div class="flex justify-center self-center bg-[url('/public/img/зебра_фон_полосы.png')] bg-no-repeat bg-center bg-cover h-full m-auto w-full">
+        <div class="grid-container mr-[366px] ml-[339px]">
             <div class="grid-item item1">
                 <img src="../public/img/блокнот_море.png" alt="">
             </div>
             <div class="grid-item item2">
-                <img src="../public/img/блокнот_зебра.png" alt="">
+                <img src="../public/img/блокнот_зебра (2).png" alt="">
             </div>
             <div class="grid-item item3">3</div>
             <div class="grid-item item4">
-                <img src="../public/img/графит.png" alt="">
-                <img src="../public/img/точиолка.png" alt="">
-                <img src="../public/img/завиток_top.png" alt="">
-                <img src="../public/img/завиток_niz.png" alt="">
+                <img src="../public/img/графит.png" alt="" class="base-image">
+                <img src="../public/img/точиолка.png" alt="" class="toc_image">
+                <img src="../public/img/завиток_top.png" alt="" class="overlay-image">
+                <img src="../public/img/завиток_niz.png" alt="" class="overlay-image">
             </div>
-            <div class="grid-item item5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ratione rerum suscipit ad error quas commodi quasi similique. Reprehenderit reiciendis tempora pariatur perspiciatis incidunt sed id quod numquam nesciunt perferendis.</div>
+            <div class="grid-item item5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ratione rerum
+                suscipit ad error quas commodi quasi similique. Reprehenderit reiciendis tempora pariatur perspiciatis
+                incidunt sed id quod numquam nesciunt perferendis.</div>
             <div class="grid-item item6">
                 <img src="../public/img/блокнот_горы.png" alt="">
             </div>
@@ -26,7 +28,7 @@
             <div class="grid-item item8">8</div>
             <div class="grid-item item9">9</div>
             <div class="grid-item item10">10</div>
-            
+
         </div>
     </div>
 </template>
