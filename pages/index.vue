6bd344dd166f34daceaf1f4f76a49f670946cc97
Furajkova Alena <Furajkova@mail.ru>
2025-01-21 09:17:13 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,35 @@
 <template>
-    <div class="flex justify-center self-center bg-[url('/public/img/зебра_фон_полосы.png')] bg-no-repeat bg-center bg-cover h-full m-auto w-full">
-        <div class="grid-container mr-[366px] ml-[339px]">
+    <div
+        class="flex justify-center self-center bg-[url('/public/img/зебра_фон_полосы.png')] bg-no-repeat bg-right bg-contain h-full w-screen">
+        <div class="grid-container">
             <div class="grid-item item1">
                 <img src="../public/img/блокнот_море.png" alt="">
             </div>
             <div class="grid-item item2">
                 <img src="../public/img/блокнот_зебра (2).png" alt="">
             </div>
-            <div class="grid-item item3">3</div>
             <div class="grid-item item4">
                 <img src="../public/img/графит.png" alt="" class="base-image">
                 <img src="../public/img/точиолка.png" alt="" class="toc_image">
-                <img src="../public/img/завиток_top.png" alt="" class="overlay-image">
-                <img src="../public/img/завиток_niz.png" alt="" class="overlay-image">
+                <img src="../public/img/завиток_top.png" alt="" class="overlay-image image1">
+                <img src="../public/img/завиток_niz.png" alt="" class="overlay-image image2">
             </div>
-            <div class="grid-item item5">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ratione rerum
-                suscipit ad error quas commodi quasi similique. Reprehenderit reiciendis tempora pariatur perspiciatis
-                incidunt sed id quod numquam nesciunt perferendis.</div>
+            <div class="grid-item item5">
+                <blockquote class="quote">
+                    <p>"Мы все, как карандаши.
+                        Каждый рисует свою судьбу. <br>
+                        Один ломается, другой тупит, <br>
+                        а кто-то затачивается и движется вперёд".</p>
+                    <cite class="cite">Светлана Кременецкая</cite>
+                </blockquote>
+            </div>
             <div class="grid-item item6">
                 <img src="../public/img/блокнот_горы.png" alt="">
             </div>
             <div class="grid-item item7">
-                <img src="../public/img/завиток_большой.png" alt="">
-                <img src="../public/img/карандаш_top.png" alt="">
-                <img src="../public/img/карандаш.png" alt="">
+                    <img src="../public/img/завиток_большой.png" alt="" class="next-image big">
+                    <img src="../public/img/карандаш_top.png" alt="" class="next-image top">
+                    <img src="../public/img/карандаш.png" alt="" class="next-image bottom">
             </div>
             <div class="grid-item item8">8</div>
             <div class="grid-item item9">9</div>
