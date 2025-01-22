65909770f46b9d4bb6b23497b0639f18c891cc5c
Furajkova Alena <Furajkova@mail.ru>
2025-01-22 05:04:40 +0300

    *

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="flex justify-center self-center bg-[url('/public/img/зебра_фон_полосы.png')] bg-no-repeat bg-right  h-full w-screen max-w-full overflow-hidden">
-        <div class="grid-container relative">
+    <div class="phone ">
+        <div class="grid-container">
             <div class="grid-item item1">
-                <img src="../public/img/блокнот_море.png" alt="">
+                <img src="../public/img/блокнот_море.png" alt="Image1">
+                <img src="../public/img/блокнот_горы.png" alt="Image2">
             </div>
-            <div class=" flex justify-center items-center">
-                <img src="../public/img/блокнот_зебра (2).png" alt="" class="absolute inset-0 mt-16 mx-auto">
+            <div class="grid-item item2">
+                <img src="../public/img/блокнот_зебра (2).png" alt="Image 2">
             </div>
             <div class="grid-item item4">
                 <img src="../public/img/графит.png" alt="" class="base-image">
@@ -23,28 +24,44 @@
                 </blockquote>
             </div>
             <div class="grid-item item6">
-                <img src="../public/img/блокнот_горы.png" alt="">
+                <img src="../public/img/завиток_большой.png" alt="" class="next-image big">
+                <img src="../public/img/карандаш_top.png" alt="" class="next-image top">
+                <img src="../public/img/карандаш.png" alt="" class="next-image bottom">
             </div>
-            <div class="grid-item item7">
-                    <img src="../public/img/завиток_большой.png" alt="" class="next-image big">
-                    <img src="../public/img/карандаш_top.png" alt="" class="next-image top">
-                    <img src="../public/img/карандаш.png" alt="" class="next-image bottom">
-            </div>
-            <div class="grid-item item8">8</div>
-            <div class="grid-item item9">9</div>
-            <div class="grid-item item10">10</div>
-
         </div>
     </div>
 </template>
 
 
-
-
-<!-- <blockquote class="blockquote-7">
-            <p>"Мы все, как карандаши. <br>
-                Каждый рисует свою судьбу. <br>
-                Один ломается, другой тупит, <br>
-                а кто-то затачивается и движется вперёд".</p>
-            <cite class="cite">Светлана Кременецкая</cite>
-        </blockquote> -->
+<!-- <div class="grid-item item1">
+    <img src="../public/img/блокнот_море.png" alt="">
+</div>
+<div class=" flex justify-center items-center">
+    <img src="../public/img/блокнот_зебра (2).png" alt="" class="absolute inset-0 mt-16 mx-auto">
+</div>
+<div class="grid-item item4">
+    <img src="../public/img/графит.png" alt="" class="base-image">
+    <img src="../public/img/точиолка.png" alt="" class="toc_image">
+    <img src="../public/img/завиток_top.png" alt="" class="overlay-image image1">
+    <img src="../public/img/завиток_niz.png" alt="" class="overlay-image image2">
+</div>
+<div class="grid-item item5">
+    <blockquote class="quote">
+        <p>"Мы все, как карандаши.
+            Каждый рисует свою судьбу. <br>
+            Один ломается, другой тупит, <br>
+            а кто-то затачивается и движется вперёд".</p>
+        <cite class="cite">Светлана Кременецкая</cite>
+    </blockquote>
+</div>
+<div class="grid-item item6">
+    <img src="../public/img/блокнот_горы.png" alt="">
+</div>
+<div class="grid-item item7">
+        <img src="../public/img/завиток_большой.png" alt="" class="next-image big">
+        <img src="../public/img/карандаш_top.png" alt="" class="next-image top">
+        <img src="../public/img/карандаш.png" alt="" class="next-image bottom">
+</div>
+<div class="grid-item item8">8</div>
+<div class="grid-item item9">9</div>
+<div class="grid-item item10">10</div> -->
