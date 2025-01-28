1b5563ebe9231e14276a096eae49df2f381cfb85
Furajkova Alena <furajkova@mail.ru>
2025-01-28 16:39:21 +0300

    *

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   devServer: {
     host: '0.0.0.0',
   },
+
   app: {
     cdnURL: 'https://fenix1521.ru/'
   },
+
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
 
   modules: ['@pinia/nuxt'],
   pinia: {
     storesDirs: ['./stores/**'],
   },
+
 })
