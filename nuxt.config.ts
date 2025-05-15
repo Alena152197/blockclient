d8681eb40ccd2bac825ce2707e218bbe31f1728a
Furajkova Alena <Furajkova@mail.ru>
2025-05-16 01:11:00 +0300

    *

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,9 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
   },
 
-  app: {
-    cdnURL: 'https://fenix1521.ru/'
-  },
+  // app: {
+  //   cdnURL: 'https://fenix1521.ru/'
+  // },
 
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
